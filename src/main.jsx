26cfba8c19ec95c804085d8fcd62f9fecd3bd2c8
Anuj2508 <anuj.y@ecodedash.com>
2025-08-29 16:22:43 +0530

    Resolved the basename:

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <BrowserRouter basename="/easeesqueezy"> */}
-    <App />
-    {/* </BrowserRouter> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
