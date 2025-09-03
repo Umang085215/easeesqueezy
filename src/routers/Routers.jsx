7bcb6634c2912e15b023f941ddfc51e4f813fa0c
Anuj2508 <anuj.y@ecodedash.com>
2025-09-03 17:21:28 +0530

    Fixing the router

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "../easeeSqueezyUser/pages/Home";
 import AddProduct from "../easeeSqueezyAdmin/pages/AddProduct";
 import Dashboard from "../easeeSqueezyAdmin/pages/Dashboard";
