d24364547f1d7569524840e928df3b9ccfd23b0c
Anuj2508 <anuj.y@ecodedash.com>
2025-09-11 17:17:23 +0530

    Added icon in slider description

--- src/easeeSqueezyUser/components/Hero.jsx
+++ src/easeeSqueezyUser/components/Hero.jsx
@@ -292,7 +292,7 @@ const Hero = () => {
                   transition={{ duration: 0.7 }}
                   viewport={{ once: false }}
                 >
-                  Fresh, Healthy, and Natural Juices
+                  Fresh, Healthy, and Natural Juices🥤🥤
                 </motion.h2>
                 <motion.h3
                   className="font-semibold text-sm sm:text-base md:text-xl text-green-800"
@@ -311,5 +311,4 @@ const Hero = () => {
     </>
   );
 };
-
 export default Hero;
