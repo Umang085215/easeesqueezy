f0c4d27986ba68b5caed4f2f233d932ba7deb239
Anuj2508 <anuj.y@ecodedash.com>
2025-09-18 18:43:58 +0530

    Updates

--- assets/AdminPublicRoute-Ckjy_8SY.js
+++
@@ -1,1 +0,0 @@
-import{j as t,N as a}from"./index-BkecdaOP.js";const i=({children:e})=>localStorage.getItem("adminToken")?t.jsx(a,{to:"/admin",replace:!0}):e;export{i as default};
---
+++ assets/AdminPublicRoute-CscVsE5-.js
@@ -0,0 +1,1 @@
+import{j as t,N as a}from"./index-BnCbwZkm.js";const i=({children:e})=>localStorage.getItem("adminToken")?t.jsx(a,{to:"/admin",replace:!0}):e;export{i as default};
